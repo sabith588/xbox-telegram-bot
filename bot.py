32b69019bd144bca9397c99dfe35304b72222a71
sabith588 <155866208+sabith588@users.noreply.github.com>
2026-08-15 11:57:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
+import os
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    ContextTypes,
+)
+
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
